367206785385349c7946864a20a1ddcb575056f8
s0heil20 <soheil.mh2000@gmail.com>
2023-06-07 19:39:29 +0330

    add DetailedBookActivity

---
+++ app/src/main/java/edu/sharif/bookstore/DetailedBookActivity.java
@@ -0,0 +1,14 @@
+package edu.sharif.bookstore;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailedBookActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_book_layout);
+    }
+}
--- app/src/main/java/edu/sharif/bookstore/MainActivity.java
+++ app/src/main/java/edu/sharif/bookstore/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, SearchActivity.class));
-
-        startActivity(new Intent(this, MainMenuActivity.class));
+        startActivity(new Intent(this, DetailedBookActivity.class));
+//        startActivity(new Intent(this, SearchActivity.class));
+// startActivity(new Intent(this, MainMenuActivity.class));
     }
 }
