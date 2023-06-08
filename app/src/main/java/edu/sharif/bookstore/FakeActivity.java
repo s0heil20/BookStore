98301cec4a46e2cbff07655c062eb0dc3d375633
nedaei79 <nedaei79@gmail.com>
2023-06-08 23:11:17 +0330

    Added "Nav Bar using extension only."

--- app/src/main/java/edu/sharif/bookstore/FakeActivity.java
+++ app/src/main/java/edu/sharif/bookstore/FakeActivity.java
@@ -1,14 +1,25 @@
 package edu.sharif.bookstore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class FakeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import edu.sharif.bookstore.utils.NavBarActivity;
+
+public class FakeActivity extends NavBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fake);
+        setContentView(R.layout.nav_fake);
+        handleParentView(R.layout.nav_fake);
     }
 }
