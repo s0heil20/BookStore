be99fc9705e5d01bf57fb15dc0bb5ee6d2fb2000
nedaei79 <nedaei79@gmail.com>
2023-06-08 11:57:00 +0330

    Revert "Added SignUp/SignIn and Drawer Menu."
    This reverts commit e3d6b5d3ff2b8492d8d63b451170fa696291b1f9.

--- app/src/main/java/edu/sharif/bookstore/MainActivity.java
+++ app/src/main/java/edu/sharif/bookstore/MainActivity.java
@@ -1,83 +1,22 @@
 package edu.sharif.bookstore;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    protected DrawerLayout drawer;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        drawer = findViewById(R.id.drawerLayout);
-        NavigationView navigationView = findViewById(R.id.navView);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_drawer_open,R.string.nav_drawer_close);
-
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-
-//        startActivity(new Intent(this, SignUpSignInActivity.class));
-//        startActivity(new Intent(this, FinalizeOrderActivity.class));
+        startActivity(new Intent(this, FinalizeOrderActivity.class));
 //        startActivity(new Intent(this, DetailedBookActivity.class));
 //        startActivity(new Intent(this, SearchActivity.class));
 // startActivity(new Intent(this, MainMenuActivity.class));
 //        startActivity(new Intent(this, SearchActivity.class));
 
 //        startActivity(new Intent(this, MainMenuActivity.class));
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.home:
-                startActivity(new Intent(this, SignUpSignInActivity.class));
-                break;
-            case R.id.cart:
-                Toast.makeText(this, "cart", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.settings:
-                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.cartHistory:
-                Toast.makeText(this, "cart history", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.account:
-                Toast.makeText(this, "account", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.favorite:
-                Toast.makeText(this, "favorite", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
